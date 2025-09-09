fd4ab05cc2d0a87aea90d7ff2bf1d7ee0e1fa3e5
Harrison Zuleta Montoya <124377148+harrisonzm@users.noreply.github.com>
2025-09-09 00:51:25 -0500

    create cantantes respository  structure

---
+++ backend/src/app/repository/cantantes.ts
@@ -0,0 +1,21 @@
+import { Cantante, postCantante , updateCantante } from '../models/cantantes';
+
+export const getCantantes = ():Cantante[]=>{
+    return [];
+}
+
+export const getCantante = (id:number): Cantante=>{
+    return {};
+}
+
+export const postCantante = (data:postCantante):Cantante=>{
+    return
+}
+
+export const updateCantante = (data:updateCantante):Cantante=>{
+    return
+}
+
+export const deleteCantante = (data:updateCantante):Cantante=>{
+    return
+}
