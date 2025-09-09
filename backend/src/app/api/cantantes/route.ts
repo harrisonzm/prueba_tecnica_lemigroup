a6cf86cdeb2f5df178c8f19cb9d7b3ce7ce05551
Harrison Zuleta Montoya <124377148+harrisonzm@users.noreply.github.com>
2025-09-09 00:51:51 -0500

    create initial routes

--- backend/src/app/api/cantantes/route.ts
+++ backend/src/app/api/cantantes/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string }}): Promise<NextResponse>{
+export async function GET(req: NextRequest): Promise<NextResponse>{
     return NextResponse.json({
         cantantes: "canta"
     })
 }
 
 
-export async function POST(req: NextRequest, { params }: { params: { id: string }}): Promise<NextResponse>{
+export async function POST(req: NextRequest): Promise<NextResponse>{
     return NextResponse.json({
         cantantes: "canta"
     })
